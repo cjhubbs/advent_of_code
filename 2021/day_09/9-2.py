c33c563a38dd9a331c0edd1d80c81d35c4672b93
Chris Hubbs <chris@chrishubbsdesign.com>
2021-12-20 14:13:15 -0600

    Cleaned up day 9 part 2

--- 2021/day_09/9-2.py
+++ 2021/day_09/9-2.py
@@ -1,3 +1,5 @@
+import numpy
+
 def is_lowest(arr,height,width, row,col):
   cur_is_lower = 1
   cur_val = arr[row][col]
@@ -15,7 +17,20 @@ def is_lowest(arr,height,width, row,col):
       cur_is_lower = 0
   return cur_is_lower
     
-
+def explore_basin(r,c,sea,found):
+  row = r
+  col = c 
+  found[r][c] = 1
+  if r > 0 and not found[r-1][c] and not sea[r-1][c] == 9:
+    found = explore_basin(r-1,c,sea,found)
+  if r < len(sea)-1 and not found[r+1][c] and not sea[r+1][c] == 9:
+    found = explore_basin(r+1,c,sea,found)
+  if c > 0 and not found[r][c-1] and not sea[r][c-1] == 9:
+    found = explore_basin(r,c-1,sea,found)
+  if c < len(sea[0])-1 and not found[r][c+1] and not sea[r][c+1] == 9:
+    found = explore_basin(r,c+1,sea,found)
+  return found
+    
 sea = []
 f = open("input9.txt","r")
 for line in f:
@@ -33,4 +48,13 @@ for r in range(height):
     if is_lowest(sea,height-1,width-1,r,c):
       low_points.append([r,c])
 
-print("low points are", low_points)
+basin_sizes = []
+for p in low_points:
+  found = numpy.zeros((height,width),dtype=int)
+  found = explore_basin(p[0],p[1],sea,found)
+  basin_sizes.append(numpy.count_nonzero(found))
+
+basin_sizes.sort(reverse=True)
+print(basin_sizes)
+result = basin_sizes[0]*basin_sizes[1]*basin_sizes[2]
+print("result is:", result)
