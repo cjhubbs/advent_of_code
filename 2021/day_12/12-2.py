0896497a84c8e7c4a8c3d59127d4e4c443d8d248
Chris Hubbs <chris@chrishubbsdesign.com>
2022-05-12 13:55:41 -0500

    Solved 2021 day 12

---
+++ 2021/day_12/12-2.py
@@ -0,0 +1,50 @@
+nodes = {}
+paths = {}
+
+
+def log_path(p):
+  t = "".join(p) + ",end"
+  if t in paths.keys():
+    paths[t] += 1
+  else:
+    paths[t] = 1
+
+def no_lower_visited_twice(path):
+  for n in nodes.keys():
+    if not n.isupper():
+      if path.count(n) > 1:
+        return False      
+  return True 
+
+def traverse(path):
+  node = path[-1]
+  for n in nodes[node]:
+    if n == 'end':
+      log_path(path)
+    elif n == 'start':
+      pass   
+    elif n.isupper() or path.count(n) == 0 or no_lower_visited_twice(path):
+      path.append(n)
+      traverse(path) 
+               
+  path.pop(-1)
+  return
+
+f = open("input12.txt", "r")
+for line in f:
+  p = line.strip().split("-")
+  if p[0] in nodes.keys():
+    nodes[p[0]].append(p[1])
+  else:
+    nodes[p[0]] = [p[1]]
+  if p[1] in nodes.keys():
+    nodes[p[1]].append(p[0])
+  else:
+    nodes[p[1]] = [p[0]]
+
+print(nodes)
+path = [ 'start' ]
+
+traverse(path)
+
+print(len(paths.keys()))
