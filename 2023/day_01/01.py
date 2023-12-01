052344ffd21307762ed8c25a4407960790aa9861
Chris Hubbs <chris.hubbs@collins.com>
2023-12-01 12:37:17 -0600

    day 01 cleanup

--- 2023/day_01/01.py
+++ 2023/day_01/01.py
@@ -9,29 +9,19 @@ class CalibrationDoc():
     with open(self.filename) as f:
       self.lines = f.read().splitlines()
 
-  def get_first_digit(self,line,accept_names):
+  def get_digit(self,line,accept_names,names):
     for i in range(len(line)):
       if line[i].isdigit():
         return line[i]
       elif accept_names:
-        for d in range(len(self.names)):
-          if line[i:i+len(self.names[d])] == self.names[d]:
+        for d in range(len(names)):
+          s = line[i:i+len(names[d])]
+          if s == names[d]:
             return d 
 
-  def get_last_digit(self,line,accept_names):
-    reversed_line = line[::-1]
-    for i in range(len(line)):
-      if reversed_line[i].isdigit():
-        return reversed_line[i]
-      elif accept_names:
-        for d in range(len(self.names)):
-          s = reversed_line[i:i+len(self.names_reversed[d])]
-          if s == self.names_reversed[d]:
-            return d 
-            
   def assemble_first_last_number(self,line,accept_names):
-    first = self.get_first_digit(line,accept_names)
-    second = self.get_last_digit(line,accept_names)
+    first = self.get_digit(line,accept_names,self.names)
+    second = self.get_digit(line[::-1],accept_names,self.names_reversed)
     num = int(str(first) + str(second))
     return num      
 
