66536bbe47dc30da1d0cf227c7431a0f5e6adfc7
Chris Hubbs <chris.hubbs@collins.com>
2023-12-04 09:32:00 -0600

    improved AoC 2023 day 4

--- 2023/day_04/04.py
+++ 2023/day_04/04.py
@@ -1,30 +1,28 @@
+def parse_line(l):
+    items = l.split(':')
+    card_num = int(items[0].split()[1])
+    nums = items[1].split('|')
+    winners = [int(i) for i in nums[0].split()]
+    tries = [int(i) for i in nums[1].split()]
+    return card_num, winners, tries 
+
 lines = []
 #with open('04-test.txt') as f:
 with open('04-input.txt') as f:
     lines = f.read().splitlines()
 total_score = 0
-cur_card = 1
 card_counts = [0]*205
 for l in lines:
-    card_counts[cur_card] += 1
-    winners = {}
-    tokens = l.split()
-#    for i in range(2,7):
-    for i in range(2,12):
-        winners[tokens[i]] = 0
-#    for i in range(8,16):
-    for i in range(13,38):
-        if tokens[i] in winners.keys():
-            winners[tokens[i]] += 1
     num_of_winners = 0
-    for v in winners.values():
-        if v > 0:
+    card_num, winners, tries = parse_line(l)
+    card_counts[card_num] += 1
+    for t in tries:
+        if t in winners:
             num_of_winners += 1
     if num_of_winners > 0:
         total_score += 2**(num_of_winners-1)
         for n in range(num_of_winners):
-            card_counts[cur_card + n + 1] += card_counts[cur_card]
-    cur_card += 1
+            card_counts[card_num + n + 1] += card_counts[card_num]
 
 print("part 1: ", total_score)
 print("part 2: ", sum(card_counts))
