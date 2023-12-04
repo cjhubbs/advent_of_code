afb5023625b4daffb6dc35210d2aaf815655368e
Chris Hubbs <chris@Chriss-MacBook-Air.local>
2023-12-03 18:23:33 -0600

    AoC 2023 Day 3

--- 2023/day_03/03.py
+++ 2023/day_03/03.py
@@ -1,3 +1,28 @@
+class Number():
+  def __init__(self,val,row,start,end):
+    self.val = val
+    self.r = row 
+    self.s = start 
+    self.e = end 
+  
+  def value(self):
+    return self.val 
+  def row(self):
+    return self.r 
+  def start(self):
+    return self.s 
+  def end(self):
+    return self.e 
+  
+  def is_adjacent_to_point(self,row,col):
+    adj = False
+    if row == self.r and ((col == self.s - 1) or (col == self.e+1)):
+      adj = True 
+    if row == self.r-1 or row == self.r+1:
+      if col >= self.s-1 and col <= self.e+1:
+        adj = True 
+    return adj 
+
 class Schematic():
   def __init__(self, filename):
     self.filename = filename
@@ -5,7 +30,7 @@ class Schematic():
     with open(self.filename) as f:
       self.lines = f.read().splitlines()
 
-  def get_numbers_in_line(self,l):
+  def get_numbers_in_line(self,l,row):
     in_number = False
     num_string = ''
     start = -1
@@ -20,35 +45,65 @@ class Schematic():
       else:
         if in_number:
           end = i-1 
-          numbers.append({'num': int(num_string),'start': start,'end':end})
+          numbers.append(Number(int(num_string),row,start,end))
           start = -1
           end = -1
           in_number = False
           num_string = ''
+    if in_number:
+      end = i-1
+      numbers.append(Number(int(num_string),row,start,end))
     return numbers   
   
-  def is_adjacent_to_symbol(self,row,start,end):
-    row_min = max(0,row-1)
-    row_max = min(row+1,len(self.lines)+1)
-    col_min = max(0,start-1)
-    col_max = min(end+1,len(self.lines[0])+1%)
-    for r in range(row_min,row_max):
-      for c in range(col_min,col_max):
-        if (not self.lines[r][c].isdigit()) and (not self.lines[r][c]) == ".":
-          return True
+  def number_is_adjacent_to_symbol(self,n):
+    row_min = n.row()-1
+    row_max = n.row()+1
+    col_min = n.start()-1
+    col_max = n.end()+1
+    for r in range(row_min,row_max+1):
+      for c in range(col_min,col_max+1):
+        if r >= 0 and r < len(self.lines):
+          if c >= 0 and c < len(self.lines[0]):
+            if (not self.lines[r][c].isdigit()) and (not self.lines[r][c] == "."):
+              return True
     return False             
+  
+  def get_gear_points(self):
+    points = []
+    for l in range(len(self.lines)):
+      for c in range(len(self.lines[0])):
+        if self.lines[l][c] == "*":
+          points.append({'row': l,'col':c})
+    return points
+  
+  def is_adjacent_to_two_numbers(self,nums, p):
+    adjacent = []
+    for n in nums:
+      if n.is_adjacent_to_point(p['row'],p['col']):
+        adjacent.append(n.value())
+    if len(adjacent) == 2:
+      return adjacent[0],adjacent[1]
+    else:
+      return 0,0
+
   
 if __name__ == "__main__":
   
-  s = Schematic('03-test.txt')
-  #s = Schematic('03-input.txt')
+  s = Schematic('03-input.txt')
   sum = 0
+  all_nums = []
 
   for r in range(len(s.lines)):
-    nums = s.get_numbers_in_line(s.lines[r])
-    print(nums)
+    nums = s.get_numbers_in_line(s.lines[r],r)
+    all_nums.extend(nums)
     for n in nums:
-      if s.is_adjacent_to_symbol(r,n['start'],n['end']):
-        sum += n['num']
-
+      if s.number_is_adjacent_to_symbol(n):
+        sum += n.value()
   print(sum)
+
+  total_gear_ratio = 0
+  gear_points = s.get_gear_points()
+  for p in gear_points:
+    g1, g2 = s.is_adjacent_to_two_numbers(all_nums, p)
+    total_gear_ratio += (g1*g2)
+  print(total_gear_ratio)
