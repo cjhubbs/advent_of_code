77ff2063679d06659e2c7fa11f0c54769b5f3008
Chris Hubbs <chris@Chriss-MacBook-Air-2.local>
2023-12-17 19:58:55 -0600

    day 10 p1 working

--- 2023/day_10/10.py
+++ 2023/day_10/10.py
@@ -1,3 +1,4 @@
+import numpy as np 
 
 start = [0,0]
 cur = [0,0]
@@ -17,10 +18,19 @@ valid_moves = {
     'W': '-LFS'
 }
 
+offsets = {
+    "|": [[-1,0],[1,0]],
+    "-": [[0,-1],[0,1]],
+    "L": [[-1,0],[0,1]],
+    "J": [[-1,0],[0,-1]],
+    "7": [[1,0],[0,-1]],
+    "F": [[1,0],[0,1]]
+}
+
 dirs = 'NSEW'
 
 if __name__ == "__main__":
-    with open('10-test.txt') as f:
+    with open('10-input.txt') as f:
         lines = f.read().splitlines()
     row_counter = 0
     lines.insert(0,'.'* len(lines[0]))
@@ -34,15 +44,23 @@ if __name__ == "__main__":
     print(start[0],start[1])
     cur = start 
     step_counter = 0
+    for m in valid_moves:
+        possible_pos = [cur[0] + move_offset[m][0],cur[1] + move_offset[m][1]]
+        if possible_pos != prev and lines[possible_pos[0]][possible_pos[1]] in valid_moves[m]:
+            prev = cur 
+            cur = possible_pos
+            step_counter += 1
+            break 
+
     while True:
-        for m in valid_moves:
-            possible_pos = [cur[0] + move_offset[m][0],cur[1] + move_offset[m][1]]
-            if possible_pos != prev and lines[possible_pos[0]][possible_pos[1]] in valid_moves[m]:
-                prev = cur 
-                cur = possible_pos
-                print(cur)
-                step_counter += 1 
-                break    
-        if cur == start and prev[0] != -1:
+        possible_offsets = offsets[lines[cur[0]][cur[1]]]
+        if np.array_equal(np.add(cur, possible_offsets[0]), prev):
+            prev = cur
+            cur = np.add(cur, possible_offsets[1])     
+        else:
+            prev = cur 
+            cur = np.add(cur, possible_offsets[0])
+        step_counter += 1            
+        if np.array_equal(cur,start) and prev[0] != -1:
             break
-    print(step_counter)
+    print(step_counter / 2)
