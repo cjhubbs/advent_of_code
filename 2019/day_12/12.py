6a47381925e17282cdf60a588e7e4926f518028c
Chris Hubbs <chris.hubbs@collins.com>
2024-01-02 12:58:34 -0600

    2019 12p1

--- 2019/day_12/12.py
+++ 2019/day_12/12.py
@@ -26,29 +26,46 @@ class Planet():
             kin += abs(self.vel[i])
         return pot * kin
 
+    def matches(self,other):
+        return self.pos == other.pos and self.vel == other.vel
+
     def print(self):
         print('pos=[',self.pos[0],',',self.pos[1],',',self.pos[2],'], vel=[',self.vel[0],',',self.vel[1],',',self.vel[2],']')
 
 if __name__ == "__main__":
 
     planets = []
+    initial_planets = []
 
     with open('12-input.txt') as f:
         lines = f.read().splitlines()
     for l in lines:
         o = re.match('.*\=(.*)\,.*\=(.*)\,.*\=(.*)\>',l)
         planets.append(Planet(int(o.group(1)),int(o.group(2)),int(o.group(3))))
+        initial_planets.append(Planet(int(o.group(1)),int(o.group(2)),int(o.group(3))))
     num_of_planets = len(planets)
 
     pairs = list(itertools.combinations(range(num_of_planets),2))
 
-    num_of_steps = 1000
-    for i in range(num_of_steps):
+    found_all_matches = False
+    found_match = [-1] * num_of_planets
+    hits = []
+    for i in range(num_of_planets):
+        hits.append({})
+    counter = 0
+    while not found_all_matches:
+        counter += 1
         for p in pairs:
             planets[p[0]].apply_gravity(planets[p[1]])
         for p in planets:
             p.apply_velocity()
-    total_energy = sum([p.energy() for p in planets])
-    print(total_energy)
+
+        #p1
+        if counter == 1000:
+            total_energy = sum([p.energy() for p in planets])
+            print("p1 total_energy: ", total_energy)
+            break
+
+
 
  
