466090a30e9b38033de2ee1a949c7f838906b254
Chris Hubbs <chris.hubbs@collins.com>
2024-07-23 13:36:14 -0500

    Aoc2018 day 5

--- 2018/day_5/5.py
+++ 2018/day_5/5.py
@@ -2,17 +2,16 @@ import string
 
 other_case = {'A': 'a','B':'b','C':'c','D':'d','E':'e','F':'f','G':'g','H':'h','I':'i','J':'j','K':'k','L':'l','M':'m','N':'n','O':'o','P':'p','Q':'q','R':'r','S':'s','T':'t','U':'u','V':'v','W':'w','X':'x','Y':'y','Z':'z','a': 'A','b':'B','c':'C','d':'D','e':'E','f':'F','g':'G','h':'H','i':'I','j':'J','k':'K','l':'L','m':'M','n':'N','o':'O','p':'P','q':'Q','r':'R','s':'S','t':'T','u':'U','v':'V','w':'W','x':'X','y':'Y','z':'Z'}
 
+reducible_pairs = ['Aa','aA','Bb','bB','Cc','cC','Dd','dD','Ee','eE','Ff','fF','Gg','gG','Hh','hH','Ii','iI','Jj','jJ','Kk','kK','Ll','lL','Mm','mM','Nn','nN','Oo','oO','Pp','pP','Qq','qQ','Rr','rR','Ss','sS','Tt','tT','Uu','uU','Vv','vV','Ww','wW','Xx','xX','Yy','yY','Zz','zZ']
+
 def reduce(chain):
-    index = 0
     while 1:
-        if chain[index] == other_case[chain[index+1]]:
-            chain = chain[:index] + chain[index+2:]
-            index = 0
-        else:
-            index += 1
-        if index == len(chain)-1:
-            break 
-    return len(chain)    
+        start_len = len(chain)
+        for p in reducible_pairs:
+            chain = chain.replace(p,"")
+        if len(chain) == start_len:
+            return len(chain)
+
 
 f = open("input-5.txt", "r")
 chain = f.read().strip()
