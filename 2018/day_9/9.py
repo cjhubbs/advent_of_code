4b701f4491365e8fb2594b1c2b14594bb734ada0
Chris Hubbs <chris.hubbs@collins.com>
2024-07-31 11:23:36 -0500

    Aoc2018 day 9

---
+++ 2018/day_9/9.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+def next_index(cur, circle):
+    length = len(circle)
+    if length < 3:
+        return 1
+    else:
+        tgt = cur + 2
+        if tgt <= length:
+            return tgt 
+        else:
+            return 1
+
+
+def play_game(num_players, last_marble):
+    circle = deque([0])
+    cur = 0
+    scores = [0] * num_players
+    cur_player = 1
+    for i in range(1,last_marble+1):
+        if i % 23 == 0:
+            circle.rotate(7)
+            scores[cur_player] += i + circle.pop() 
+            circle.rotate(-1)
+        else:
+            circle.rotate(-1)
+            circle.append(i)
+        cur_player += 1
+        if cur_player == num_players:
+            cur_player = 0
+    return max(scores)
+
+if __name__ == "__main__":
+    players = 462
+    marbles = 71938
+    #p1
+    high_score = play_game(players,marbles)
+    print(high_score)
+    #p2
+    marbles = marbles * 100
+    high_score = play_game(players,marbles)
+    print(high_score)    
