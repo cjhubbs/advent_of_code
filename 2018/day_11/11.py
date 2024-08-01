517ba46c90a2a076fbc9f3256b730d40ad8a60cb
Chris Hubbs <chris.hubbs@collins.com>
2024-08-01 08:54:49 -0500

    Aoc2018 day 11 p2\

--- 2018/day_11/11.py
+++ 2018/day_11/11.py
@@ -3,6 +3,7 @@ import numpy
 grid_sn = 8868
 grid = numpy.zeros((301,301),dtype=int)
 total_power = numpy.zeros((301,301),dtype=int)
+tp3d = numpy.zeros((301,301,301),dtype=int)
 
 for x in range(1,301):
     for y in range(1,301):
@@ -16,9 +17,19 @@ for x in range(1,301):
             hunds_char = str(power_level)[-3]
             grid[x][y] = int(hunds_char) - 5
 
+#p1
 for x in range(1,298):
     for y in range(1,298):
         total_power[x][y] = numpy.sum(grid[x:x+3,y:y+3])
 
 idx = numpy.unravel_index(numpy.argmax(total_power),total_power.shape)
+print(idx)
+
+#p2
+for z in range(1,301):
+    for x in range(1,301-z):
+        for y in range(1,301-z):
+            tp3d[x][y][z] = numpy.sum(grid[x:x+z,y:y+z])
+
+idx = numpy.unravel_index(numpy.argmax(tp3d),tp3d.shape)
 print(idx)
