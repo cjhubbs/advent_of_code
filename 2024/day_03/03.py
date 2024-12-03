ea5a6505a7ad9819381c24690dccba2174eab80f
Chris Hubbs <chris.hubbs@collins.com>
2024-12-03 12:28:42 -0600

    AoC 2024 day 3

--- 2024/day_03/03.py
+++ 2024/day_03/03.py
@@ -22,3 +22,18 @@ if __name__ == "__main__":
     with open(filename) as f:
         lines = f.read().splitlines()
     f.close()
+    pattern = 'mul\((\d{1,3}),(\d{1,3})\)|(do)\(\)|(don)\'t\(\)'
+
+    total = 0
+    enabled = True
+    for l in lines:
+        matches = re.findall(pattern,l)
+        for m in matches:
+            if m[3] == 'don':
+                enabled = False 
+            elif m[2] == 'do':
+                enabled = True 
+            else:
+                if enabled:
+                    total += int(m[0]) * int(m[1])
+    print(total)
