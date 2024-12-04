50564a2f3d3b1028cd4f1693102bd907b1510e46
Chris Hubbs <chris.hubbs@collins.com>
2024-12-04 11:53:48 -0600

    AoC 2024 day 4 p2

--- 2024/day_04/04.py
+++ 2024/day_04/04.py
@@ -30,6 +30,16 @@ def check_xmas(r,c,lines):
                count += 1
     return count
 
+def check_x(r,c,lines):
+    if 0 < r < (rows-1) and 0 < c < (cols-1):
+        if ((lines[r+1][c+1] == "M" and lines[r+1][c-1] == "M" and lines[r-1][c+1] == "S" and lines[r-1][c-1] == "S") or
+            (lines[r+1][c+1] == "S" and lines[r+1][c-1] == "S" and lines[r-1][c+1] == "M" and lines[r-1][c-1] == "M") or
+            (lines[r+1][c+1] == "M" and lines[r+1][c-1] == "S" and lines[r-1][c+1] == "M" and lines[r-1][c-1] == "S") or
+            (lines[r+1][c+1] == "S" and lines[r+1][c-1] == "M" and lines[r-1][c+1] == "S" and lines[r-1][c-1] == "M")):
+           return 1
+        
+    return 0
+    
 if __name__ == "__main__":
 
     #p1
@@ -40,9 +50,14 @@ if __name__ == "__main__":
     rows = len(lines)
     cols = len(lines[0])
 
-    count = 0
+    p1_count = 0
+    p2_count = 0
     for r in range(rows):
         for c in range(cols):
             if lines[r][c] == "X":
-                count += check_xmas(r,c,lines)
-    print(count)
+                p1_count += check_xmas(r,c,lines)
+            if lines[r][c] == "A":
+                p2_count += check_x(r,c,lines)
+    print(p1_count)
+    print(p2_count)
+
